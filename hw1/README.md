4e6260142345bcf5911a66f7783173bb4da9b8b7
Orlando, Ding <or.ding@sap.com>
2018-10-31 09:15:14 +0800

    orlando - for updates of run_expert.py and readme.md

--- hw1/README.md
+++ hw1/README.md
@@ -39,10 +39,198 @@ put mjkey.txt into ~/.mujoco/mjkey.txt
 
 ### 2. Exercise
 #### 2.1 [Section 2] Behavior Cloning
+##### Part 1
 * Generation of expert behaviors $o_t$ and $a_t$
 ```bash
-python run_expert.py experts/Hopper-v2.pkl Hopper-v2 --render --num_rollouts 1
+python run_expert.py experts/Humanoid-v2.pkl Humanoid-v2 --num_rollouts 500 --only_expert_generate 0
 ```
 Reward performance analysis:
-1. average reward: $\frac{\sum_{t} r_{t}}{T}$ = 3781.212325396108, suppose $\left\lVert t \right\rVert = T$
-2. standard deviation of reward: $\sigma(r_{t})$ = 0.0
+1. average reward: $\frac{\sum_{t} r_{t}}{T}$ = 10357.515730550287, suppose $\left\lVert t \right\rVert = T$
+2. standard deviation of reward: $\sigma(r_{t})$ = 686.9833818443245
+function [expert_policy_out] in 266.8122 s
+
+* Behavior Cloning
+```bash
+python run_expert.py experts/Humanoid-v2.pkl Humanoid-v2 --num_rollouts 1 --only_expert_generate 1
+```
+
+##### Part 2
+* Generation of expert behaviors $o_t$ and $a_t$
+```bash
+python run_expert.py experts/Humanoid-v2.pkl Humanoid-v2 --num_rollouts 500 --only_expert_generate 0
+```
+mean return 10347.679984145881
+std of return 721.6504464500745
+function [expert_policy_out] in 669.2001 s
+
+* Behavior Cloning [BC epochs=10]
+```bash
+python run_expert.py experts/Humanoid-v2.pkl Humanoid-v2 --num_rollouts 50 --only_expert_generate 1
+```
+Epoch=0, loss=0.11469971
+Epoch=5, loss=0.027275419
+Final Epoch=9, loss=0.019465791
+mean return 7647.900418809727
+std of return 3303.701292393463
+function [bc_policy_estimate] in  86.9479 s
+
+```bash
+python run_expert.py experts/Humanoid-v2.pkl Humanoid-v2 --num_rollouts 100 --only_expert_generate 1
+```
+Epoch=0, loss=0.11426015
+Epoch=5, loss=0.027157418
+Final Epoch=9, loss=0.019502923
+mean return 4349.717127544281
+std of return 3184.253907784524
+function [bc_policy_estimate] in  97.6978 s
+
+* Behavior Cloning [BC epochs=60]
+```bash
+python run_expert.py experts/Humanoid-v2.pkl Humanoid-v2 --num_rollouts 50 --only_expert_generate 1
+```
+Epoch=0, loss=0.115757085
+Epoch=5, loss=0.027166951
+Epoch=10, loss=0.018360632
+Epoch=15, loss=0.01457547
+Epoch=20, loss=0.012546841
+Epoch=25, loss=0.011068988
+Epoch=30, loss=0.01018011
+Epoch=35, loss=0.009535846
+Epoch=40, loss=0.00901775
+Epoch=45, loss=0.008615256
+Epoch=50, loss=0.0082463585
+Epoch=55, loss=0.008115762
+Final Epoch=59, loss=0.0077691893
+mean return 10138.193242617175
+std of return 1338.987515723146
+function [bc_policy_estimate] in 253.3601 s
+
+* Behavior Cloning [BC epochs=80, full gradient descent]
+```bash
+python run_expert.py experts/Humanoid-v2.pkl Humanoid-v2 --num_rollouts 50 --only_expert_generate 1
+```
+Epoch=0, loss=1.8137274
+Epoch=5, loss=0.44362494
+Epoch=10, loss=0.3252377
+Epoch=15, loss=0.26969045
+Epoch=20, loss=0.23329984
+Epoch=25, loss=0.21012451
+Epoch=30, loss=0.19155973
+Epoch=35, loss=0.17868455
+Epoch=40, loss=0.16747464
+Epoch=45, loss=0.15881208
+Epoch=50, loss=0.15144132
+Epoch=55, loss=0.14515686
+Epoch=60, loss=0.13950676
+Epoch=65, loss=0.13464123
+Epoch=70, loss=0.13017614
+Epoch=75, loss=0.12608676
+Final Epoch=79, loss=0.123026684
+mean return 318.2433393156144
+std of return 73.9023671335878
+function [bc_policy_estimate] in 287.5026 s
+
+* Behavior Cloning [BC epochs=80, 1024 batch gradient descent]
+```bash
+python run_expert.py experts/Humanoid-v2.pkl Humanoid-v2 --num_rollouts 50 --only_expert_generate 1
+```
+Epoch=0, loss=0.11540921
+Epoch=5, loss=0.027249534
+Epoch=10, loss=0.018459626
+Epoch=15, loss=0.0145938685
+Epoch=20, loss=0.012500723
+Epoch=25, loss=0.01119253
+Epoch=30, loss=0.010436625
+Epoch=35, loss=0.009746236
+Epoch=40, loss=0.009210576
+Epoch=45, loss=0.008742314
+Epoch=50, loss=0.008551183
+Epoch=55, loss=0.00812455
+Epoch=60, loss=0.007905282
+Epoch=65, loss=0.0077865347
+Epoch=70, loss=0.007558631
+Epoch=75, loss=0.007488649
+Final Epoch=79, loss=0.0072339475
+mean return 10176.550300507024
+std of return 1367.6098174785814
+function [bc_policy_estimate] in 309.3935 s
+
+* Behavior Cloning [BC epochs=100, 1024 batch gradient descent]
+```bash
+python run_expert.py experts/Humanoid-v2.pkl Humanoid-v2 --num_rollouts 50 --only_expert_generate 1
+```
+Epoch=0, loss=0.11540921
+Epoch=5, loss=0.027249534
+Epoch=10, loss=0.018459626
+Epoch=15, loss=0.0145938685
+Epoch=20, loss=0.012500723
+Epoch=25, loss=0.01119253
+Epoch=30, loss=0.010436625
+Epoch=35, loss=0.009746236
+Epoch=40, loss=0.009210576
+Epoch=45, loss=0.008742314
+Epoch=50, loss=0.008551183
+Epoch=55, loss=0.00812455
+Epoch=60, loss=0.007905282
+Epoch=65, loss=0.0077865347
+Epoch=70, loss=0.007558631
+Epoch=75, loss=0.007488649
+Final Epoch=79, loss=0.0072339475
+mean return 10176.550300507024
+std of return 1367.6098174785814
+function [bc_policy_estimate] in 309.3935 s
+
+* Behavior Cloning [BC epochs=80, full gradient descent, iteration=5 for full gradient descent]
+```bash
+python run_expert.py experts/Humanoid-v2.pkl Humanoid-v2 --num_rollouts 50 --only_expert_generate 1
+```
+Epoch=0, loss=0.53019905
+Epoch=5, loss=0.19239265
+Epoch=10, loss=0.14397344
+Epoch=15, loss=0.12132259
+Epoch=20, loss=0.10594936
+Epoch=25, loss=0.09446361
+Epoch=30, loss=0.08573013
+Epoch=35, loss=0.07880429
+Epoch=40, loss=0.07269504
+Epoch=45, loss=0.068423145
+Epoch=50, loss=0.06358309
+Epoch=55, loss=0.06048623
+Epoch=60, loss=0.05686547
+Epoch=65, loss=0.054783233
+Epoch=70, loss=0.05154618
+Epoch=75, loss=0.049049344
+Final Epoch=79, loss=0.048039272
+mean return 529.7355482636622
+std of return 127.12297541557157
+function [bc_policy_estimate] in 1390.5618 s
+
+* Behavior Cloning [BC epochs=100, 2048 gradient descent]
+```bash
+python run_expert.py experts/Humanoid-v2.pkl Humanoid-v2 --num_rollouts 50 --only_expert_generate 1
+```
+Epoch=0, loss=0.14715466
+Epoch=5, loss=0.032662433
+Epoch=10, loss=0.021775546
+Epoch=15, loss=0.016877564
+Epoch=20, loss=0.014108729
+Epoch=25, loss=0.012313774
+Epoch=30, loss=0.011078215
+Epoch=35, loss=0.010290821
+Epoch=40, loss=0.009271965
+Epoch=45, loss=0.008937109
+Epoch=50, loss=0.008514404
+Epoch=55, loss=0.00820086
+Epoch=60, loss=0.0077426825
+Epoch=65, loss=0.007561854
+Epoch=70, loss=0.0073805456
+Epoch=75, loss=0.0071492125
+Final Epoch=79, loss=0.0071405536
+mean return 9985.341163047473
+std of return 1806.3608494965674
+function [bc_policy_estimate] in 266.3633 s
+
+* Dagger [epochs=60, 1024 batch gradient descent]
+```bash
+python run_expert.py experts/Humanoid-v2.pkl Humanoid-v2 --num_rollouts 50 --only_expert_generate 2
+```
--- hw1/run_expert.py
+++ hw1/run_expert.py
@@ -3,8 +3,12 @@
 """
 Code to load an expert policy and generate roll-out data for behavioral cloning.
 Example usage:
-    python run_expert.py experts/Humanoid-v2.pkl Humanoid-v2 --render --num_rollouts 1
-    python run_expert.py experts/HalfCheetah-v2.pkl HalfCheetah-v2 --render --num_rollouts 1
+    1. Expert generation
+    python run_expert.py experts/Humanoid-v2.pkl Humanoid-v2 --num_rollouts 200 --only_expert_generate 0
+    2. Training for BC
+    python run_expert.py experts/Humanoid-v2.pkl Humanoid-v2 --num_rollouts 1 --only_expert_generate 1
+    3. Training for Dagger
+    python run_expert.py experts/Humanoid-v2.pkl Humanoid-v2 --num_rollouts 50 --only_expert_generate 2
 
 Author of this script and included expert policies: Jonathan Ho (hoj@openai.com)
 """
@@ -17,7 +21,75 @@ import tf_util
 import gym
 import load_policy
 
+from tensorflow.layers import dense, batch_normalization
+from tensorflow.contrib.layers import fully_connected
+from tensorflow.losses import mean_squared_error
+from tensorflow.train import AdamOptimizer
 
+from time import time
+from functools import wraps
+from sklearn.utils import gen_batches
+
+def timeit(func):
+    @wraps(func)
+    def timed(*args, **kwargs):
+        startTime = time() 
+        result = func(*args, **kwargs)
+        print("function [{}] in {:8.4f} s".format(func.__name__, time() - startTime))
+        return result
+    return timed
+
+def neuralNetwork(expert_data, lr):
+    """[construct shared neural Network]
+    
+    Arguments:
+        expert_data {[dictionary of expert data]} -- [exported expert data]
+        lr {[float]} -- [learning rate]
+    
+    Returns:
+        [Tensor tuple] -- [op-optimizatio operation, loss, O-obsevation tensor, A-action tensor, is_training, X_out-predicted action tensor]
+    """
+    # construct full-connected layers
+    O_shape = list(expert_data['observations'].shape)
+    A_shape = list(expert_data['actions'].shape)
+    assert O_shape[0] == A_shape[0]
+    # to tensor shape
+    A_fshape=[-1 if i==0 else elem for i, elem in enumerate(A_shape)]
+    O_shape[0]=None; A_shape[0]=None
+    # sign
+    is_training=tf.placeholder(tf.bool)
+    # construct fully_connected layers
+    O = tf.placeholder(tf.float32, shape=O_shape)
+    A = tf.placeholder(tf.float32, shape=A_shape)
+    # layer dimension
+    layer_sizes = [int(elem * A_shape[-1]) for elem in [50, 30, 10, 1]]
+    ## Why with batch_normal, we can't even reach the reward maximum?
+    # X = batch_normalization(O, training=is_training)
+    X = O
+    for i, layer_size in enumerate(layer_sizes):
+        if i != len(layer_sizes) - 1:
+            X = dense(X, units=layer_size, activation='tanh')
+            # X = fully_connected(X, num_outputs=layer_size, activation_fn=tf.nn.tanh)
+        else:
+            X = dense(X, units=layer_size, activation=None)
+            # X = fully_connected(X, num_outputs=layer_size, activation_fn=None)
+    X_out = tf.reshape(X, A_fshape)
+    loss = mean_squared_error(predictions=X_out, labels=A)
+    op = AdamOptimizer(learning_rate=lr).minimize(loss)
+    return op, loss, O, A, is_training, X_out
+
+def loadExpertOA():
+    """[load expert data from data on disk]
+    
+    Returns:
+        [dict] -- [expert data]
+    """
+    with open(os.path.join(expert_data_dir, args.envname + '.pkl'), 'rb') as f:
+        expert_data = pickle.load(f)
+    assert expert_data != None
+    return expert_data
+
+@timeit
 def expert_policy_out():
     """[Using the existing expert policy to generate expert trajectory to expert_data_dir folder]
     """
@@ -65,39 +137,15 @@ def expert_policy_out():
         with open(os.path.join(expert_data_dir, args.envname + '.pkl'), 'wb') as f:
             pickle.dump(expert_data, f, pickle.HIGHEST_PROTOCOL)
 
-def bc_policy_estimate(lr: float=1e-3, epoches: int=60, iterations: int=100):
+@timeit
+def bc_policy_estimate(lr: float=1e-3, epoches: int=80, iterations: int=5):
     """[Using behaviorial cloning to do policy estimate and use estimated policy to do task]
         Arguments:
             lr {[float]} -- [learning rate value]
     """
-    from tensorflow.layers import dense, batch_normalization
-    from tensorflow.losses import mean_squared_error
-    from tensorflow.train import AdamOptimizer
-    
-    with open(os.path.join(expert_data_dir, args.envname + '.pkl'), 'rb') as f:
-        expert_data = pickle.load(f)
-    assert expert_data != None
-
+    expert_data = loadExpertOA()
     # construct full-connected layers
-    O_shape = list(expert_data['observations'].shape)
-    A_shape = list(expert_data['actions'].shape)
-    assert O_shape[0] == A_shape[0]
-    # to tensor shape
-    A_fshape=[-1 if i==0 else elem for i, elem in enumerate(A_shape)]
-    O_shape[0]=None; A_shape[0]=None
-    # sign
-    is_training=tf.placeholder(tf.bool)
-    # construct fully_connected layers
-    O = tf.placeholder(tf.float32, shape=O_shape)
-    A = tf.placeholder(tf.float32, shape=A_shape)
-    # layer dimension
-    layer_sizes = [int(elem * A_shape[-1]) for elem in [3, 2, 1]]
-    X = batch_normalization(O, training=is_training)
-    for layer_size in layer_sizes:
-        X = dense(X, units=layer_size)
-    X_out = tf.reshape(X, A_fshape)
-    loss = mean_squared_error(X_out, A)
-    op = AdamOptimizer(learning_rate=lr).minimize(loss)
+    op, loss, O, A, is_training, X_out = neuralNetwork(expert_data, lr)
 
     with tf.Session() as sess:
         # 1. Training phase
@@ -105,40 +153,148 @@ def bc_policy_estimate(lr: float=1e-3, epoches: int=60, iterations: int=100):
         for epoch in range(epoches):
             indexes = np.arange(expert_data['observations'].shape[0])
             np.random.shuffle(indexes)
-            for _ in range(iterations):
-                _, loss_val = sess.run([op, loss], feed_dict={O: expert_data['observations'][indexes], 
-                                                              A: expert_data['actions'][indexes], 
-                                                              is_training: True})
-            if epoch % 20 == 0:
-                print("Epoch=%s, loss=%s" % (epoch, loss_val))
-        print("Final Epoch=%s, loss=%s" % (epoches-1, loss_val))
+            # refer to https://github.com/tensorflow/tensorflow/issues/13847 and https://towardsdatascience.com/how-to-use-dataset-in-tensorflow-c758ef9e4428
+            loss_vals = []
+            batch_size = 2048
+            # batch_size = len(indexes)
+            for batch in gen_batches(len(indexes), batch_size):
+                if batch_size == len(indexes):
+                    for _ in range(iterations):
+                        _, loss_val = sess.run([op, loss], feed_dict={O: expert_data['observations'][indexes[batch]], 
+                                                    A: expert_data['actions'][indexes[batch]], 
+                                                    is_training: True
+                                                    })
+                else: 
+                    _, loss_val = sess.run([op, loss], feed_dict={O: expert_data['observations'][indexes[batch]], 
+                                                                A: expert_data['actions'][indexes[batch]], 
+                                                                is_training: True
+                                                                })
+                loss_vals.append(loss_val)
+            if epoch % 5 == 0:
+                print("Epoch=%s, loss=%s" % (epoch, np.average(loss_vals)))
+        print("Final Epoch=%s, loss=%s" % (epoches-1, np.average(loss_vals)))
 
         # 2. Prediction phase
         returns = []
-        observations = []
-        actions = []
-        obs = env.reset()
-        done = False
-        totalr = 0.
-        steps = 0
-        while not done:
-            # trajectory sampling
-            action = sess.run(X_out, feed_dict={O: obs[None,:], 
-                                                  is_training: False})
-            observations.append(obs)
-            actions.append(action)
-            obs, r, done, _ = env.step(action)
-            totalr += r
-            steps += 1
-            if args.render:
-                env.render()
-            if steps % 100 == 0: print("%i/%i"%(steps, max_steps))
-            if steps >= max_steps:
-                break
-        returns.append(totalr)
+
+        # using the same rollout
+        for i in range(args.num_rollouts):
+            print('iter', i)
+            obs = env.reset()
+            done = False
+            totalr = 0.
+            steps = 0
+            while not done:
+                # trajectory sampling
+                action = sess.run(X_out, feed_dict={O: obs[None,:], 
+                                                    is_training: False
+                                                    })
+                obs, r, done, _ = env.step(action)
+                totalr += r
+                steps += 1
+                if args.render:
+                    env.render()
+                if steps % 100 == 0: print("%i/%i"%(steps, max_steps))
+                if steps >= max_steps:
+                    break
+            returns.append(totalr)
         print('returns', returns)
         print('mean return', np.mean(returns))
         print('std of return', np.std(returns))
+
+@timeit
+def dagger_policy_estimate(lr: float=1e-3, epoches: int=60, iterations: int=5, dagger_loop: int=5):
+    """[dataset aggregation algorithm]
+    
+    Keyword Arguments:
+        lr {float} -- [learning rate value] (default: {1e-3})
+        epoches {int} -- [training epoches] (default: {60})
+        iterations {int} -- [iteration time for each epoch] (default: {100})
+    """
+    expert_data = loadExpertOA()
+    # construct full-connected layers
+    op, loss, O, A, is_training, X_out = neuralNetwork(expert_data, lr)
+    # expert policy to use a human labeled action
+    expert_fn = load_policy.load_policy(args.expert_policy_file)
+    
+    for dagger_i in range(dagger_loop):
+        with tf.Session() as sess:
+            # 1. Training phase to learn pi(theta)
+            sess.run(tf.global_variables_initializer())
+            tf_util.initialize()
+            for epoch in range(epoches):
+                indexes = np.arange(expert_data['observations'].shape[0])
+                np.random.shuffle(indexes)
+                loss_vals = []
+                batch_size = 1024
+                # batch_size = len(indexes)
+                for batch in gen_batches(len(indexes), batch_size):
+                    if batch_size == len(indexes):
+                        for _ in range(iterations):
+                            _, loss_val = sess.run([op, loss], feed_dict={O: expert_data['observations'][indexes[batch]], 
+                                                        A: expert_data['actions'][indexes[batch]], 
+                                                        is_training: True
+                                                        })
+                    else: 
+                        _, loss_val = sess.run([op, loss], feed_dict={O: expert_data['observations'][indexes[batch]], 
+                                                                    A: expert_data['actions'][indexes[batch]], 
+                                                                    is_training: True
+                                                                    })
+                    loss_vals.append(loss_val)
+                if epoch % 10 == 0:
+                    print("Epoch=%s, loss=%s" % (epoch, np.average(loss_vals)))
+            print("Final Epoch=%s, loss=%s" % (epoches-1, np.average(loss_vals)))
+            
+            # 2. Generate D_pi from D(pi(theta)) -> Neural Network : only increase 1 roll outs
+            for i in range(args.num_rollouts):
+                obs = env.reset()
+                done = False
+                steps = 0
+                d_pi_obs = []; d_pi_action = []
+                while not done:
+                    # trajectory sampling
+                    action = sess.run(X_out, feed_dict={O: obs[None,:], 
+                                                        is_training: False})
+                    obs, r, done, _ = env.step(action)
+                    # using expert action by expert_fn
+                    action_by_expert = expert_fn(obs[None,:])
+                    d_pi_obs.append(obs)
+                    d_pi_action.append(action_by_expert)
+                    if steps >= max_steps:
+                        break
+            
+                # 3. update expoert_data
+                expert_data['observations'] = np.concatenate((expert_data['observations'], np.array(d_pi_obs)),axis=0)
+                expert_data['actions'] = np.concatenate((expert_data['actions'], np.array(d_pi_action)),axis=0)
+        
+            if dagger_i == dagger_loop - 1:
+                # the last loop to estimate rewards
+                returns = []
+
+                # using the same rollout
+                for i in range(args.num_rollouts):
+                    print('iter', i)
+                    obs = env.reset()
+                    done = False
+                    totalr = 0.
+                    steps = 0
+                    while not done:
+                        # trajectory sampling
+                        action = sess.run(X_out, feed_dict={O: obs[None,:], 
+                                                            is_training: False})
+                        obs, r, done, _ = env.step(action)
+                        totalr += r
+                        steps += 1
+                        if args.render:
+                            env.render()
+                        if steps % 100 == 0: print("%i/%i"%(steps, max_steps))
+                        if steps >= max_steps:
+                            break
+                    returns.append(totalr)
+                print('returns', returns)
+                print('mean return', np.mean(returns))
+                print('std of return', np.std(returns))
+
 
 
 if __name__ == '__main__':
@@ -148,7 +304,7 @@ if __name__ == '__main__':
     parser.add_argument('envname', type=str)
     parser.add_argument('--render', action='store_true')
     parser.add_argument("--expert_data", type=str, default="expert_data", help="Export data folder")
-    parser.add_argument("--skip_expert_generate", type=bool, default=True)
+    parser.add_argument("--only_expert_generate", type=int, default=0)
     parser.add_argument("--max_timesteps", type=int)
     parser.add_argument('--num_rollouts', type=int, default=20, help='Number of expert roll outs')
     args = parser.parse_args()
@@ -160,9 +316,14 @@ if __name__ == '__main__':
     max_steps = args.max_timesteps or env.spec.timestep_limit
 
     # Preparation of expert policy to generate archived expert trajectory
-    if not args.skip_expert_generate:
+    if args.only_expert_generate == 0:
         # if not skip expert generate, then run it out
         expert_policy_out()
-
-    # Comparsion using BC to esimate policy
-    bc_policy_estimate()
+    elif args.only_expert_generate == 1:
+        # Comparsion using BC to esimate policy
+        bc_policy_estimate()
+    elif args.only_expert_generate == 2:
+        # Comparsion using Dagger to esimate policy
+        dagger_policy_estimate()
+    else:
+        raise ValueError("Invalid argument parameter: args.only_expert_generate is only in [0, 1, 2]")
